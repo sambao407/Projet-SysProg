b66d7ff0cfc441616b375d20ed7725f1998f0c6f
AlexandreTavernier <alexandredidierrene.tavernier@viacesi.fr>
2018-12-05 18:59:28 +0100

    préparation tests

---
+++ ProjetMasterChefInfo/ProjetMasterChefInfoTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjetMasterChefInfoTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjetMasterChefInfoTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5b4be02-6cfc-420e-9f66-c275f3188bef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
