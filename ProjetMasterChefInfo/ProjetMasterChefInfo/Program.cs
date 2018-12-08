f9a5c69de0e007e51af9f4bedb8ac5900c63a980
sambao407 <sambao407@gmail.com>
2018-12-08 15:45:32 +0100

    test

--- ProjetMasterChefInfo/ProjetMasterChefInfo/Program.cs
+++ ProjetMasterChefInfo/ProjetMasterChefInfo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjetMasterChefInfo.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace ProjetMasterChefInfo
     {
         static void Main(string[] args)
         {
+            TestConnexion p = new TestConnexion();
+            p.connect();
+            Console.ReadLine();
         }
     }
 }
